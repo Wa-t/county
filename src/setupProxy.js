9400b5ece04f394c62d5af45f1dacfcede2eecf0
wstreet <wstreet7@outlook.com>
2020-05-11 01:29:09 +0800

    fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,19 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
-<<<<<<< HEAD
   app.use('/bang',
     createProxyMiddleware({
       target: 'https://www.clgnews.com',
       changeOrigin: true,
     })
   )
-=======
-    app.use('/bang',
-        createProxyMiddleware({
-            target: 'https://www.clgnews.com',
-            changeOrigin: true,
-        })
-    )
->>>>>>> 7f2d06f4cd32f27da17fe08754881da436570007
 }
